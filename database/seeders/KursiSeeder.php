4ccf34ae8bf629014d7a3df54c110a5266001857
erlannn <ahmaderlanweb@gmail.com>
2026-08-08 00:49:30 +0700

    Update migrations & seeders

--- database/seeders/KursiSeeder.php
+++ database/seeders/KursiSeeder.php
@@ -15,21 +15,29 @@ class KursiSeeder extends Seeder
         // Kursi untuk Jadwal 1
         $kursiJadwal1 = ['1A', '1B', '2A', '2B', '3A', '3B'];
         foreach ($kursiJadwal1 as $nomor) {
-            Kursi::create([
-                'id_jadwal' => 1,
-                'nomor_kursi' => $nomor,
-                'status' => $nomor === '1A' ? 'Terisi' : 'Kosong',
-            ]);
+            Kursi::firstOrCreate(
+                [
+                    'id_jadwal' => 1,
+                    'nomor_kursi' => $nomor,
+                ],
+                [
+                    'status' => $nomor === '1A' ? 'Terisi' : 'Kosong',
+                ]
+            );
         }
 
         // Kursi untuk Jadwal 2
         $kursiJadwal2 = ['1A', '1B', '2A', '2B', '3A', '3B'];
         foreach ($kursiJadwal2 as $nomor) {
-            Kursi::create([
-                'id_jadwal' => 2,
-                'nomor_kursi' => $nomor,
-                'status' => 'Kosong',
-            ]);
+            Kursi::firstOrCreate(
+                [
+                    'id_jadwal' => 2,
+                    'nomor_kursi' => $nomor,
+                ],
+                [
+                    'status' => 'Kosong',
+                ]
+            );
         }
     }
 }
