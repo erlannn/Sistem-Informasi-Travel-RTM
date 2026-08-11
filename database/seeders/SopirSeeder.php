ceea24b4bee007751062faf73a9773df14aaab36
erlannn <ahmaderlanweb@gmail.com>
2026-08-11 11:48:05 +0700

    Update Seeder Supir

--- database/seeders/SopirSeeder.php
+++ database/seeders/SopirSeeder.php
@@ -56,14 +56,118 @@ class SopirSeeder extends Seeder
         );
 
         $u3 = User::firstOrCreate(
-            ['email' => 'joko@rtmtravel.com'],
+            ['email' => 'Nanda@rtmtravel.com'],
             [
-                'name' => 'Joko Widodo',
+                'name' => 'Nanda',
                 'password' => Hash::make('password123'),
             ]
         );
         if (!$u3->hasRole('Sopir')) {
             $u3->assignRole($roleSopir);
         }
+
+        Sopir::firstOrCreate(
+            ['nama' => 'Nanda'],
+            [
+                'no_hp' => '085374074741',
+                'alamat' => 'Nagari Sijunjung',
+            ]
+        );
+
+        $u4 = User::firstOrCreate(
+            ['email' => 'Eri@rtmtravel.com'],
+            [
+                'name' => 'Eri',
+                'password' => Hash::make('password123'),
+            ]
+        );
+        if (!$u4->hasRole('Sopir')) {
+            $u4->assignRole($roleSopir);
+        }
+
+        Sopir::firstOrCreate(
+            ['nama' => 'Eri'],
+            [
+                'no_hp' => '085376155659',
+                'alamat' => 'Nagari Pematang Panjang',
+            ]
+        );
+
+
+        $u5 = User::firstOrCreate(
+            ['email' => 'Tomy@rtmtravel.com'],
+            [
+                'name' => 'Tomy',
+                'password' => Hash::make('password123'),
+            ]
+        );
+        if (!$u5->hasRole('Sopir')) {
+            $u5->assignRole($roleSopir);
+        }
+
+        Sopir::firstOrCreate(
+            ['nama' => 'Tomy'],
+            [
+                'no_hp' => '082391157508',
+                'alamat' => 'Nagari Sijunjung',
+            ]
+        );
+
+        $u6 = User::firstOrCreate(
+            ['email' => 'Fahmi@rtmtravel.com'],
+            [
+                'name' => 'Fahmi',
+                'password' => Hash::make('password123'),
+            ]
+        );
+        if (!$u6->hasRole('Sopir')) {
+            $u6->assignRole($roleSopir);
+        }
+
+        Sopir::firstOrCreate(
+            ['nama' => 'Fahmi'],
+            [
+                'no_hp' => '082387359942',
+                'alamat' => 'Nagari Sijunjung',
+            ]
+        );
+
+        $u7 = User::firstOrCreate(
+            ['email' => 'Yoga@rtmtravel.com'],
+            [
+                'name' => 'Yoga',
+                'password' => Hash::make('password123'),
+            ]
+        );
+        if (!$u7->hasRole('Sopir')) {
+            $u7->assignRole($roleSopir);
+        }
+
+        Sopir::firstOrCreate(
+            ['nama' => 'Yoga'],
+            [
+                'no_hp' => '082283632616',
+                'alamat' => 'Nagari Sijunjung',
+            ]
+        );
+
+        $u8 = User::firstOrCreate(
+            ['email' => 'Puji@rtmtravel.com'],
+            [
+                'name' => 'Puji',
+                'password' => Hash::make('password123'),
+            ]
+        );
+        if (!$u8->hasRole('Sopir')) {
+            $u8->assignRole($roleSopir);
+        }
+
+        Sopir::firstOrCreate(
+            ['nama' => 'Puji'],
+            [
+                'no_hp' => '085263008933',
+                'alamat' => 'Nagari Muaro',
+            ]
+        );
     }
 }
